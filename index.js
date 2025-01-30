2c540b3ff8be3ec48ba912b5f1d97c2d728864d2
iiuperboyii <aaronjomon24@gmail.com>
2025-01-30 09:29:21 +0530

    completed task

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
-const port = 3010;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch(err => {
+    console.error(`Error connecting to database: ${err}`);
+  });
 
 app.use(express.static('static'));
 
@@ -10,6 +20,6 @@ app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
 });
